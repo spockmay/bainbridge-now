1d46e9777360729c4cc62af61f0f379f2c50430c
Ryan May <spockmay@users.noreply.github.com>
2025-09-09 17:58:01 -0400

    The Geauga library also uses ics for their calendar

--- bainbridgenow.py
+++ bainbridgenow.py
@@ -10,5 +10,14 @@ for event in events:
     event.zip_code = "44023"
 scraped_events.append(events)
 
+# Bainbridge Library
+events = scrape_ics(
+    "https://geaugalibrary.libcal.com/ical_subscribe.php?src=p&cid=9901&cam=5019"
+)
+for event in events:
+    event.event_type = "LIBRARY"
+    event.zip_code = "44023"
+scraped_events.append(events)
+
 for event in scraped_events:
     print(event)
