a6ee3b05e178cc29afb9b704d91a63b980633029
Ryan May <spockmay@users.noreply.github.com>
2025-09-09 23:11:09 -0400

    Add support for Kenston schools

--- bainbridgenow.py
+++ bainbridgenow.py
@@ -8,6 +8,14 @@ load_dotenv()  # loads .env file into environment
 
 scraped_events = []
 
+
+def any_word_in(phrase: str, words: List[str]) -> bool:
+    for word in words:
+        if word in phrase:
+            return True
+    return False
+
+
 # # Bainbridge Township
 # events = scrape_ics("http://bainbridgetwp.com/events/list/?ical=1")
 # for event in events:
@@ -24,6 +32,20 @@ scraped_events = []
 #     event.zip_code = "44023"
 # scraped_events.extend(events)
 
+# Kenston Schools
+# events = scrape_ics(
+#     "https://thrillshare-cmsv2.services.thrillshare.com/api/v4/o/22349/cms/events/generate_ical?filter_ids&section_ids"
+# )
+# filters = ["theater", "play", "concert", "silver bells"]
+# filt_events = []
+# for event in events:
+#     if any_word_in(event.name.lower(), filters):
+#         event.event_type = "SCHOOL"
+#         event.zip_code = "44023"
+#         filt_events.append(event)
+
+# scraped_events.extend(filt_events)
+
 # # CVCC - https://www.cvcc.org/events/calendar
 # timestamp_ms = int(time.time() * 1000)
 # url = f"https://www.cvcc.org/events_upcoming?t={timestamp_ms}&rendermode=json&version=3&limit=20"
@@ -39,10 +61,10 @@ scraped_events = []
 # events = convert_llm_json_to_events(events_json, url)
 # scraped_events.extend(events)
 
-# Bainbridge Historical Society
-url = "https://bainbridgehistoricalsociety.org/events/"
-events = parse_bainbridge_events(url)
-scraped_events.extend(events)
+# # Bainbridge Historical Society
+# url = "https://bainbridgehistoricalsociety.org/events/"
+# events = parse_bainbridge_events(url)
+# scraped_events.extend(events)
 
 for event in scraped_events:
     print(event)
